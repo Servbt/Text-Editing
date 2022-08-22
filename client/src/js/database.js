255827ce66c43fb8c77b482f3ff4345b4315d221
AREZVANI95 <105834594+Servbt@users.noreply.github.com>
2022-08-21 22:55:29 -0400

    added some logic from a previous activity to the installer.js file

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -1,4 +1,5 @@
 import { openDB } from 'idb';
+initdb();
 
 const initdb = async () =>
   openDB('jate', 1, {
@@ -12,7 +13,6 @@ const initdb = async () =>
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
 export const putDb = async (content) => {
   const jateDB = await openDB('jate', 1);
   const transaction = jateDB.transaction('store', 'readwrite'); 
@@ -22,7 +22,6 @@ export const putDb = async (content) => {
   console.log('🚀 - data saved to the database', result);
 };
 
-// TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => console.error('getDb not implemented');
 const jateDB = await openDB('jate', 1);
 const transaction = jateDB.transaction('contact', 'readonly');
@@ -31,4 +30,4 @@ const request = store.getAll();
 const result = await request;
 console.log('result.value', result);
 return result;
-initdb();
+
