0c36e7c0a134656b4d64f52f8329c81c59e1e7ea
AREZVANI95 <105834594+Servbt@users.noreply.github.com>
2022-08-21 23:44:30 -0400

    added css and bable rules to module in config file, also added static asset caching (WIP)

--- client/src-sw.js
+++ client/src-sw.js
@@ -27,4 +27,25 @@ warmStrategyCache({
 registerRoute(({ request }) => request.mode === 'navigate', pageCache);
 
 // TODO: Implement asset caching
-registerRoute();
+const cacheName = 'static-resources';
+const matchCallback = ({ request }) => {
+  console.log(request);
+  return (
+    // CSS
+    request.destination === 'style' ||
+    // JavaScript
+    request.destination === 'script'
+  );
+};
+
+registerRoute(
+  matchCallback,
+  new offlineFallback({
+    cacheName,
+    plugins: [
+      new CacheableResponsePlugin({
+        statuses: [0, 200],
+      }),
+    ],
+  })
+);
